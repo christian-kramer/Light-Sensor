8ddd17952c67f4bd12fad416334ec68e9a811c96
christian <christian.kramer@mail.com>
2018-09-24 01:40:11 -0500

    reliably working

--- firmware/Light Sensor.X/main.c
+++ firmware/Light Sensor.X/main.c
@@ -5,6 +5,8 @@
  * Created on September 20, 2018, 9:26 PM
  */
 
+#define _XTAL_FREQ 8000000
+
 // CONFIG
 #pragma config IOSCFS = 8MHZ    // Internal Oscillator Frequency Select bit (8 MHz)
 #pragma config MCPU = ON        // Master Clear Pull-up Enable bit (Pull-up enabled)
@@ -14,11 +16,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <xc.h>
 
 /*
  * 
  */
+
 
 char getLightValue()
 {
@@ -26,32 +30,74 @@ char getLightValue()
     return ~ADRES;
 }
 
+void flash()
+{
+    GPIO = 0x04;
+    __delay_ms(10);
+    GPIO = 0x00;
+}
+
+char calibrate()
+{
+    char count = 0;
+    char baseline = 0;
+    
+    while (count++ < 254)
+    {
+        char newValue = getLightValue();
+        
+        if (newValue > baseline && newValue < 249)
+        {
+            baseline = newValue;
+        }
+        
+        __delay_ms(2);
+    }
+    
+    return baseline;
+}
+
 void main() {
     
-    TRISGPIO    = 0x01;
-    ADCON0      = 0x41; //01000001
     
-    char startValue = getLightValue();
-    char count = 0;
+    OSCCALbits.FOSC4 = 0;
+    OPTION = 0b01000000;
+    TRISGPIO = 0x01;
+    ADCON0 = 0x41; //01000001
+    GPIO = 0x00;
+    ADRES = 0x00;
     
-    while (1) {
+    flash();
+    
+    char score = 50;
+    
+    char startValue = calibrate();
+    
+    flash();
+    
+    while (1)
+    {
         char currentValue = getLightValue();
-        if (currentValue > startValue + 5 && count < 100)
+        
+        
+        if (currentValue > startValue + 5 && score < 100)
         {
-            count++;
+            score++;
         }
-        if (currentValue < startValue - 5 && count > 0)
+        
+        if (currentValue < startValue - 5 && score > 0)
         {
-            count--;
+            score--;
         }
-        if (count == 100)
+        
+        if (score == 100)
         {
             GPIO = 0x04;
         }
-        if (count == 0)
+        
+        if (score == 0)
         {
             GPIO = 0x00;
         }
     }
 }
-
